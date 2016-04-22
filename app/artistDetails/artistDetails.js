5f92a2e89f1bf0ca76d109cf83eab8c4d6787866
anderson1795 <stephenanderson@Stephens-MacBook-Pro.local>
2016-04-21 22:14:40 -0400

    Put albums on artist results page

--- app/artistDetails/artistDetails.js
+++ app/artistDetails/artistDetails.js
@@ -5,20 +5,25 @@ angular.module('myApp.artistDetails', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/artistDetails/:artistId', {
     templateUrl: 'artistDetails/artistDetails.html',
-    controller: 'ArtistDetailsCtrl'
+    controller: 'ArtistDetailsCtrl',
+    css: 'artistDetails/artistDetails.css'
   });
 }])
 
 .controller('ArtistDetailsCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
     var getArtistURL = 'https://api.spotify.com/v1/artists/' + $routeParams.artistId;
-    var setResponseToModel = function(response) {
+    var setArtistResponseToModel = function(response) {
             $scope.artist = response.data;
             $scope.name = $routeParams.artistName;
         }
+    var setAlbumResponseToModel = function(response) {
+        $scope.albums = response.data.items;
+    }
     $scope.millisToMinutesAndSeconds = function millisToMinutesAndSeconds(millis) {
           var minutes = Math.floor(millis / 60000);
           var seconds = ((millis % 60000) / 1000).toFixed(0);
           return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         }
-    $http.get(getArtistURL).then(setResponseToModel);
+    $http.get(getArtistURL).then(setArtistResponseToModel);
+    $http.get(getArtistURL + '/albums').then(setAlbumResponseToModel);
 }]);
